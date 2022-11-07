a61e9c84c003fb8bec7bb4b028bc06f81ea9baa0
MingzhiXuWill <mx2091@nyu.edu>
2022-11-07 12:51:15 -0500

    11/7 Movement + Particle System

---
+++ Terratota/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3;
+
+    public ParticleSystem Gun;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Move
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(x * speed * Time.deltaTime, y * speed * Time.deltaTime, 0);
+
+        // Gun
+        float DisX = Gun.transform.position.x - Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float DisY = Gun.transform.position.y - Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+        Gun.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(-DisY, -DisX) * Mathf.Rad2Deg));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Gun.Play();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Gun.Stop();
+        }
+    }
+}
